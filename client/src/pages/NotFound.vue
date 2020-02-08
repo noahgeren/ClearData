99dd60dabeaa40c898965d465f6ee8401fa09a5b
Jace Bayless <jacebayless@gmail.com>
2020-02-08 16:18:43 -0600

    Added 404 page

---
+++ client/src/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="mainBody">
+        <d-row>
+            <d-col cols="12" class="text-center">
+                <h1 class="pt-5">404 - Oops, you shouldn't be here!</h1>
+                <h3 class="pt-2">If you believe you should have ended up somewhere else please let us know.</h3>
+            </d-col>
+        </d-row>
+    </div>
+</template>
