ca18225885a106d7b7e36ad2b9783a5470e1dfaf
Jace Bayless <jacebayless@gmail.com>
2020-02-08 21:31:22 -0600

    Updated dashboard

--- client/src/pages/Dashboard.vue
+++ client/src/pages/Dashboard.vue
@@ -5,7 +5,59 @@
                 <b-card no-body class="">
                     <b-tabs pills card vertical>
                         <!-- TAB ONE -->
-                        <b-tab title="Weekly View" active><b-card-text>
+                        <!-- <b-tab title="Weekly View" active ><b-card-text>Test</b-card-text></b-tab>-->
+                        <b-tab title="Analytics" active>
+                            <b-row>
+                                <b-col cols="2" class="text-center">
+                                    <b-dropdown :text="toggleText" variant="primary" size="lg" class="vertical-center" center>
+                                       <b-dropdown-item-button @click="toggle('Weekly')">Weekly</b-dropdown-item-button>
+                                       <b-dropdown-item-button @click="toggle('Yearly')">Yearly</b-dropdown-item-button>
+                                    </b-dropdown>       
+                                </b-col>
+                                <b-col cols="8">
+                                    <b-card-text id="graph"><plotly :data="graphData" :layout="graphLayout" :displayModeBar="true"/></b-card-text>
+                                </b-col>
+                            </b-row>
+                            <!-- Numbahs -->
+                            <b-row id="numbers" class="py-5 bg-secondary text-light">
+                                <b-col cols="12">
+                                    <h1 class="pb-4 text-center font-weight-bold">Numbers</h1>
+                                    <b-col cols="10" md="8" offset="1" offset-md="2">
+                                        <b-row>
+                                            <b-col lg="4" class="pb-4 py-lg-0 d-flex align-items-stretch">
+                                                <b-card title="Historical" class="text-dark">
+                                                    <hr/>
+                                                    <b-card-text class="flex-column">
+                                                        Hisorical numbers go here qwerqwerqerqw {{weekly[0].average}}
+                                                    </b-card-text>
+                                                    
+                                                </b-card>
+                                            </b-col>
+                                            <b-col lg="4" class="pb-4 py-lg-0 d-flex align-items-stretch">
+                                                <b-card title="Current"  class="text-dark">
+                                                    <hr/>
+                                                    <b-card-text class="flex-column">
+                                                        Current numbers go here qwerqw erqwefqfre {{weekly2.average}}
+                                                    </b-card-text>
+                                                    
+                                                </b-card>
+                                            </b-col>
+                                            <b-col lg="4" class="pb-4 py-lg-0 d-flex align-items-stretch">
+                                                <b-card title="Projected"  class="text-dark">
+                                                    <hr/>
+                                                    <b-card-text class="flex-column">
+                                                        Projected numbers go here werwerwerwerqwerqwer {{weekly3.average}}
+                                                    </b-card-text>
+                                                    
+                                                </b-card>
+                                            </b-col>
+                                        </b-row>
+                                    </b-col>
+                                </b-col>
+                            </b-row>
+                        </b-tab>
+                        
+                        <b-tab title="Weather" ><b-card-text>
                             <b-row class="pb-4">
                                 <b-col cols="5" >
                                     <b-button variant="primary" class="float-right">
@@ -32,34 +84,25 @@
                                     <hr/>
                                     <p>Predicted: <br/> ${{ day.predicted }} </p>
                                     <p>Actual: <br/> ${{ day.actual }} </p>
+                                    
                                 </b-col>
                             </b-row>
                         </b-card-text></b-tab>
-                        <b-tab title="Analytics">
-                            <b-row>
-                                <b-col cols="2" class="text-center">
-                                    <b-dropdown :text="toggleText" variant="primary" size="lg" class="vertical-center" center>
-                                       <b-dropdown-item-button @click="toggle('Weekly')">Weekly</b-dropdown-item-button>
-                                       <b-dropdown-item-button @click="toggle('Yearly')">Yearly</b-dropdown-item-button>
-                                    </b-dropdown>       
-                                </b-col>
-                                <b-col cols="8">
-                                    <b-card-text id="graph"><plotly :data="graphData" :layout="graphLayout" :displayModeBar="true"/></b-card-text>
-                                </b-col>
-                            </b-row>
-                        </b-tab>
-                        <b-tab title="Weather" ><b-card-text>Test</b-card-text></b-tab>
                         <b-tab title="Settings"><b-card-text>Accuweather is great!</b-card-text></b-tab>
                     </b-tabs>
                 </b-card>
             </b-col>
         </b-row>
+
     </div>
 </template>
 
 <script>
 import feather from "feather-icons";
 import { Plotly } from 'vue-plotly';
+import axios from 'axios';
+
+
 
 export default {
     components: {
@@ -67,38 +110,81 @@ export default {
     },
     data(){
         return {
-            weekly:[{
+            
+            weekly:[{ // historical
                 x: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
-                y: [1, 2, 3, 2, 4, 6, 10],
-                type:"scatter"
+                y: [12, 21, 12, 33, 12, 16, 24],
+                type:"scatter",
+                name: "Historical Avgs",
+                average: 123
             }],
-            yearly:[{
+
+            weekly2:{ // projected
+                x: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+                y: [10, 20, 30, 20, 40, 60, 20],
+                type:"scatter",
+                name: "Projected Avgs",
+                average:123
+            },
+
+            weekly3:{ // this week so far
+                x: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+                y: [14, 23, 22, 16],
+                type:"scatter",
+                name: "This Week",
+                average: 123
+            },
+            
+            yearly:[{ // historical
                 x: ["Janurary","February","March","April","May","June","July","August","September","Octoeber","November","December"],
-                y: [1, 0, 3, 2, 5, 4, 7, 6, 8, 10, 5, 0],
-                type:"scatter"
+                y: [15, 10, 33, 23, 52, 45, 76, 62, 81, 10, 52, 50],
+                type:"scatter",
+                name: "Historical Avgs"
             }],
+            yearly2:{ // projected
+                x: ["Janurary","February","March","April","May","June","July","August","September","Octoeber","November","December"],
+                y: [10, 0, 30, 20, 50, 40, 70, 60, 80, 100, 50, 0],
+                type:"scatter",
+                name: "Projected Avgs",
+                
+            },
+            yearly3:{ // this year so far
+                x: ["Janurary","February","March","April","May","June","July","August","September","Octoeber","November","December"],
+                y: [30, 10, 20, 24, 53],
+                type:"scatter",
+                name: "This Year",
+                
+            },
+
             weeklyLayout:{
                 title: "Weekly View",
                 xaxis: {title: "Day of Week"},
-                yaxis: {title: "Average Income"}
+                yaxis: {title: "Average Income"},
+                showlegend: true,
+                 transition: {
+                    duration: 500,
+                    easing: 'cubic-in-out'
+                    },
+                    frame: {
+                    duration: 500
+                    }
             },
             yearlyLayout:{
                 title: "Yearly View",
                 xaxis: {title: "Year of Month"},
-                yaxis: {title: "Average Income"}
+                yaxis: {title: "Average Income"},
+                showlegend: true,
+                 transition: {
+                    duration: 500,
+                    easing: 'cubic-in-out'
+                    },
+                    frame: {
+                    duration: 500
+                    }
             },
             days:[
                 {
                     day: "Sunday",
-                    date: "2/8",
-                    weather: "cloud-drizzle",
-                    tempLow: "26",
-                    tempHigh: "42",
-                    predicted: "420.69",
-                    actual: "400.00"
-                },
-                {
-                    day: "Monday",
                     date: "2/9",
                     weather: "cloud-drizzle",
                     tempLow: "26",
@@ -107,7 +193,7 @@ export default {
                     actual: "400.00"
                 },
                 {
-                    day: "Tuesday",
+                    day: "Monday",
                     date: "2/10",
                     weather: "cloud-drizzle",
                     tempLow: "26",
@@ -116,7 +202,7 @@ export default {
                     actual: "400.00"
                 },
                 {
-                    day: "Wednesday",
+                    day: "Tuesday",
                     date: "2/11",
                     weather: "cloud-drizzle",
                     tempLow: "26",
@@ -125,7 +211,7 @@ export default {
                     actual: "400.00"
                 },
                 {
-                    day: "Thursday",
+                    day: "Wednesday",
                     date: "2/12",
                     weather: "cloud-drizzle",
                     tempLow: "26",
@@ -134,7 +220,7 @@ export default {
                     actual: "400.00"
                 },
                 {
-                    day: "Friday",
+                    day: "Thursday",
                     date: "2/13",
                     weather: "cloud-drizzle",
                     tempLow: "26",
@@ -143,8 +229,17 @@ export default {
                     actual: "400.00"
                 },
                 {
-                    day: "Saturday",
+                    day: "Friday",
                     date: "2/14",
+                    weather: "cloud-drizzle",
+                    tempLow: "26",
+                    tempHigh: "42",
+                    predicted: "420.69",
+                    actual: "400.00"
+                },
+                {
+                    day: "Saturday",
+                    date: "2/15",
                     weather: "cloud-drizzle",
                     tempLow: "26",
                     tempHigh: "42",
@@ -158,6 +253,7 @@ export default {
     methods: {
         toggle(toggle){
             this.toggleText = toggle;
+
         }        
     },
     computed: {
@@ -176,9 +272,30 @@ export default {
                 return this.yearlyLayout;
             }
             return [];
-        }
+        },
     },
     mounted(){
+        
+        axios
+        .get('/api/averages/week/1')
+        .then(response => {
+            
+            for(let rep = 0; rep < 7; rep++){
+                this.weekly[0].y[rep] = (response.data[rep]);
+            }
+            
+            
+        }) 
+        .catch(error => {
+            console.log(error)
+        })
+        .finally(() => {this.loading = false;});
+
+        this.weekly.push(this.weekly2);
+        this.weekly.push(this.weekly3);
+        this.yearly.push(this.yearly2);
+        this.yearly.push(this.yearly3);
+
         feather.replace({
             'stroke-width':2
         });
--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,18 +1,62 @@
 <template>
     <b-row id="mainBody" class="bg-secondary">
         <b-col cols="12" md="4" offset-md="4" id="login">
-            <div class="my-5 p-5 rounded bg-white">
+            <b-form class="my-5 p-5 rounded bg-white" @submit.prevent="login">
                 <h1 class="text-center">Login</h1>
                 <hr/>
                 <label for="username" class="py-2" >Username:</label>
-                <b-form-input id="username" size="lg" type="text" placeholder="Username" class="mb-4"></b-form-input>
+                <b-form-input id="username" size="lg" type="text" placeholder="Username" class="mb-4" v-model="username"></b-form-input>
                 <label for="password" class="py-2">Password:</label>
-                <b-form-input id="password" size="lg" type="password" placeholder="Password" class="mb-4"></b-form-input>
-                <b-button block size="lg" variant="primary">Login</b-button>
-            </div>
+                <b-form-input id="password" size="lg" type="password" placeholder="Password" class="mb-4" v-model="password"></b-form-input>
+                <b-button type="submit" block size="lg" variant="primary">Login</b-button>
+            </b-form>
         </b-col>
     </b-row>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+    data() {
+        return{
+            username: '',
+            password: '',
+            success: false
+        }
+
+    },
+
+    methods: {
+        login(){
+            axios
+                .post("/login", {
+                    username: this.username,
+                    password: this.password
+                }, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }).then(response => {
+                    console.log(response);
+                    //either login successful or unsucessful
+                    if(response.data.success){
+                        success = true; //login successful
+                    }
+                    else{
+                        success = false; //failed to log in
+                    }
+                }).catch(error => {
+                    console.log(error);
+                })
+           
+        }
+    }
+
+}
+
+</script>
 
 <style>
     #login h1{
@@ -22,3 +66,4 @@
         font-size:16pt;
     }
 </style>
+
